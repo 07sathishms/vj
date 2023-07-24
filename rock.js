fcb6a67320899c3f9d079ab71d704907e9f1a585
07sathishms <91666613+07sathishms@users.noreply.github.com>
2023-07-24 16:25:27 +0530

    Update rock.js

--- rock.js
+++ rock.js
@@ -1,16 +1,16 @@
 function set(){
     setTimeout((()=> {
-        document.body.style.background = "linear-gradient(90deg,rgba(2, 255, 213, 0.3), rgba(255, 23, 23)),url('https://images.pexels.com/photos/235986/pexels-photo-235986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')"
+        document.body.style.background = "linear-gradient(90deg,rgba(2, 255, 213, 0.3), rgba(255, 23, 23)),url('https://wallpapercrafter.com/desktop2/818554-Music-Techno-party-cdj-pioneer-dj-djm-x-entertainment.jpg')"
        document.body.style.backgroundSize = "cover"
         document.getElementById("h1").innerHTML= "HUKUM";
     }),100);
     setTimeout((()=> {
-        document.body.style.background = "linear-gradient(90deg,rgba(148, 165, 161,0.3),rgba(224, 247, 243)),url('https://images.pexels.com/photos/235986/pexels-photo-235986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')"
+        document.body.style.background = "linear-gradient(90deg,rgba(148, 165, 161,0.3),rgba(224, 247, 243)),url('https://wallpapercrafter.com/desktop2/818554-Music-Techno-party-cdj-pioneer-dj-djm-x-entertainment.jpg')"
        document.body.style.backgroundSize = "cover"
         document.getElementById("h1").innerHTML= "TIGER KA";
     }),400);
     setTimeout((()=> {
-        document.body.style.background = "90deg,rgba(2, 455, 213, 0.3), rgba(555, 23, 23)),url('https://images.pexels.com/photos/235986/pexels-photo-235986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')"
+        document.body.style.background = "90deg,rgba(2, 455, 213, 0.3), rgba(555, 23, 23)),url('https://wallpapercrafter.com/desktop2/818554-Music-Techno-party-cdj-pioneer-dj-djm-x-entertainment.jpg')"
        document.body.style.backgroundSize = "cover"
         document.getElementById("h1").innerHTML= "HUKUM";
     }),800);
